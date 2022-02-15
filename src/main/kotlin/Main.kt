bd5685ea2a2edb9d54d360ff6bfc11ceebbf4809
mobile1211 <gregorybuttars902@gmail.com>
2022-02-15 18:20:47 +1300

    Update for 0.12

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,102 @@
 import io.appwrite.Client
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonElement
-import com.google.gson.JsonParser
 import io.appwrite.exceptions.AppwriteException
 import io.appwrite.services.Database
 import io.appwrite.services.Functions
 import io.appwrite.services.Storage
 import io.appwrite.services.Users
-import kotlinx.coroutines.runBlocking
 import java.io.File
-import kotlin.math.min
-
 
 val client = Client()
-    .setEndpoint("https://localhost/v1")
-    .setProject("YOUR PROJECT ID")
-    .setKey("YOUR API KEY")
+    .setEndpoint("https://demo.appwrite.io/v1")
     // .setJWT("jwt") // Enable this to authenticate with JWT created using client SDK
 
+val database = Database(client)
+val storage = Storage(client)
+val functions = Functions(client)
+val users = Users(client)
+
 lateinit var collectionId: String
+lateinit var documentId: String
 lateinit var fileId: String
 lateinit var userId: String
 lateinit var functionId: String
 
 suspend fun main() {
-    runBlocking {
-        createUser("kotlinplayground@appwrite.io", "user@123", "Kotlin Player")
-        createCollection()
-        uploadFile()
-        createFunction()
-    }
+    createUser("kotlinplayground@appwrite.io", "user@123", "Kotlin Player")
     listUsers()
     deleteUser()
-    listCollection()
-    addDoc()
-    listDoc()
-    deleteCollection()
-    deleteFile()
 
+    createCollection()
+    listCollections()
+
+    createDocument()
+    listDocuments()
+    deleteDocument()
+
+    deleteCollection()
+
+    createFunction()
     listFunctions()
     deleteFunction()
+
+    uploadFile()
+    listFiles()
+    deleteFile()
+}
+
+suspend fun createUser(email: String, password: String, name: String) {
+    println("Running create user API")
+
+    try {
+        val response = users.create(
+            userId = "unique()",
+            email,
+            password,
+            name
+        )
+        userId = response.id
+        println(response.toMap())
+    } catch (e: AppwriteException) {
+        println(e)
+    }
+}
+
+suspend fun listUsers() {
+    println("Running list users API")
+
+    try {
+        val response = users.list()
+        println(response.users.first().toMap())
+    } catch (e: AppwriteException) {
+        println(e)
+    }
+}
+
+suspend fun deleteUser() {
+    println("Running delete user API")
+
+    try {
+        users.delete(userId)
+        println("User deleted")
+    } catch (e: AppwriteException) {
+        println(e)
+    }
 }
 
 suspend fun createCollection() {
-    val database = Database(client)
     println("Running create collection API")
+
     try {
-        val res = database.createCollection(collectionId = "movies", name = "Movies", permission = "document", read = arrayListOf("role:all"), write = arrayListOf(
-            "role:all"))
+        val res = database.createCollection(
+            collectionId = "movies",
+            name = "Movies",
+            permission = "document",
+            read = arrayListOf("role:all"),
+            write = arrayListOf("role:all")
+        )
+
         collectionId = res.id
+
         var res2 = database.createStringAttribute(
             collectionId = collectionId,
             key = "name",
@@ -57,6 +105,7 @@ suspend fun createCollection() {
             default = "",
             array = false
         )
+
         var res3 = database.createIntegerAttribute(
             collectionId = collectionId,
             key = "release_year",
@@ -65,15 +114,47 @@ suspend fun createCollection() {
             max = 9999,
             array = false
         )
+
+        var res4 = database.createFloatAttribute(
+            collectionId = collectionId,
+            key = "rating",
+            required = true,
+            min = 0.0,
+            max = 99.99,
+            array = false
+        )
+
+        var res5 = database.createBooleanAttribute(
+            collectionId = collectionId,
+            key = "kids",
+            required = true,
+            array = false
+        )
+
+        var res6 = database.createEmailAttribute(
+            collectionId = collectionId,
+            key = "email",
+            required = false,
+            default = "",
+            array = false
+        )
+
+        var res7 = database.createIndex(
+            collectionId = collectionId,
+            key = "name_email_idx",
+            type = "fulltext",
+            attributes = listOf("name", "email")
+        )
+
         println(res.toMap())
     } catch (e:AppwriteException) {
-        println(e.message);
+        println(e.message)
     }
 }
 
-suspend fun listCollection() {
-    val database = Database(client);
+suspend fun listCollections() {
     println("Running list collection API")
+
     try {
         val res = database.listCollections()
         println(res.collections.first().toMap())
@@ -83,80 +164,72 @@ suspend fun listCollection() {
 }
 
 suspend fun deleteCollection() {
-    val database = Database(client)
     println("Running delete collection API")
+
     try {
-        database.deleteCollection(collectionId = collectionId)
+        database.deleteCollection(collectionId)
         println("collection deleted")
     } catch (e:AppwriteException) {
         println(e.message)
     }
 }
 
-suspend fun addDoc() {
-    val database = Database(client);
+suspend fun createDocument() {
     println("Running Add Document API")
+
     try {
         val res = database.createDocument(
             collectionId = collectionId,
             documentId = "unique()",
-            data = mapOf("name" to "Spider Man", "release_year" to 1920),
-            read = arrayListOf("role:all"),
-            write = arrayListOf("role:all"))
+            data = mapOf(
+                "name" to "Spider Man",
+                "release_year" to 1920,
+                "rating" to 99.5,
+                "kids" to false
+            ),
+            read = listOf("role:all"),
+            write = listOf("role:all")
+        )
+
+        documentId = res.id
+
         println(res.toMap())
     } catch (e:AppwriteException) {
         println(e.message)
     }
 }
 
-suspend fun listDoc() {
-    val database = Database(client)
+suspend fun listDocuments() {
     println("Running List Document API")
+
     try {
-        val response = database.listDocuments(collectionId = collectionId);
+        val response = database.listDocuments(collectionId)
         println(response.documents.first().toMap())
     } catch (e:AppwriteException) {
         println(e.message)
     }
 }
 
-suspend fun uploadFile() {
-    val storage = Storage(client);
-    println("Running Upload File API")
-    val file =
-        File("./nature.jpg")
-    try {
-        val response = storage.createFile(
-            fileId = "unique()",
-            file = file, //multipart file
-            read = arrayListOf("*"),
-            write = arrayListOf(),
-        )
-        fileId = response.id
-        println(response.toMap())
-    } catch (e:AppwriteException) {
-        println(e.message)
-    }
-}
+suspend fun deleteDocument() {
+    println("Running Delete Document API")
 
-suspend fun deleteFile() {
-    val storage = Storage(client)
-    println("Running Delete File API")
     try {
-        storage.deleteFile(fileId = fileId)
-        println("File deleted")
+        database.deleteDocument(collectionId, documentId)
+        println("Document deleted")
     } catch (e:AppwriteException) {
         println(e.message)
     }
 }
 
 suspend fun createFunction() {
-    val functions = Functions(client)
     println("Running Create Function API")
     try {
         val res = functions.create(
             functionId = "unique()",
-            name = "test function", execute = arrayListOf(), runtime = "dart-2.12")
+            name = "Test Function",
+            execute = listOf("role:all"),
+            runtime = "dart-2.14"
+        )
         functionId = res.id
         println(res.toMap())
     } catch (e:AppwriteException) {
@@ -165,8 +238,8 @@ suspend fun createFunction() {
 }
 
 suspend fun listFunctions() {
-    val functions = Functions(client)
     println("Running List Functions API")
+
     try {
         val res = functions.list()
         println(res.functions.first().toMap())
@@ -176,51 +249,52 @@ suspend fun listFunctions() {
 }
 
 suspend fun deleteFunction() {
-    val functions = Functions(client);
     println("Running Delete Function API")
+
     try {
-        functions.delete(functionId = functionId)
+        functions.delete(functionId)
         println("Function deleted")
     } catch (e:AppwriteException) {
         println(e.message)
     }
 }
 
-suspend fun listUsers() {
-    println("Running list users API")
-    val users = Users(client)
-    try {
-        val response = users.list()
-        println(response.users.first().toMap())
-    } catch (e: AppwriteException) {
-        println(e)
-    }
-}
+suspend fun uploadFile() {
+    println("Running Upload File API")
 
-suspend fun createUser(email: String, password: String, name: String) {
-    println("Running create user API")
-    val users = Users(client)
+    val file = File("./nature.jpg")
     try {
-        val response = users.create(
-            userId = "unique()",
-            email = "email@example.com",
-            password = "password",
+        val response = storage.createFile(
+            fileId = "unique()",
+            file = file, // Multipart file
+            read = listOf("role:all"),
+            write = listOf(),
         )
-        userId = response.id
+        fileId = response.id
         println(response.toMap())
-    } catch (e: AppwriteException) {
-        println(e)
+    } catch (e:AppwriteException) {
+        println(e.message)
     }
 }
 
-suspend fun deleteUser() {
-    var users = Users(client)
-    println("Running delete user API")
+suspend fun listFiles() {
+    println("Running List File API")
+
     try {
-        val response = users.delete(
-            userId = userId        )
-        println("user deleted")
-    } catch (e: AppwriteException) {
-        println(e)
+        val res = storage.listFiles()
+        println(res.files.first().toMap())
+    } catch (e:AppwriteException) {
+        println(e.message)
+    }
+}
+
+suspend fun deleteFile() {
+    println("Running Delete File API")
+
+    try {
+        storage.deleteFile(fileId)
+        println("File deleted")
+    } catch (e:AppwriteException) {
+        println(e.message)
     }
 }
